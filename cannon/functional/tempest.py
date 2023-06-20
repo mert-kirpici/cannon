5acd36dfd7090d40cb620624356926c6e223bfd2
Mert Kırpıcı <mert.kirpici@canonical.com>
2023-06-20 16:34:58 +0800

    chore: add dummy module and test for coverage
    Signed-off-by: Mert Kırpıcı <mert.kirpici@canonical.com>

---
+++ cannon/functional/tempest.py
@@ -0,0 +1,5 @@
+"""Module for providing an interface to tempest."""
+
+
+def run() -> None:
+    """Call tempest run."""
---
+++ tests/unit/test_cannon.py
@@ -0,0 +1,6 @@
+from cannon.functional import tempest
+
+
+def test_tempest_run():
+    tempest.run()
+    assert True
